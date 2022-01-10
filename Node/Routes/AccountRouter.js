3cacb5c5bdb6df15c6624b398ce77d7cb466911a
oonn22 <dropthepuck12@gmail.com>
2022-01-10 18:50:17 -0500

    Route now returns an empty account if account not in DAG.

--- Node/Routes/AccountRouter.js
+++ Node/Routes/AccountRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Account = require('../../DAG/DataClasses/Account.js');
 
 router.get('/', getAccount);
 
@@ -7,6 +8,10 @@ async function getAccount(req, res, next) {
     let address = req.query.address;
     let DAG = req.app.locals.DAG;
     let account = await DAG.getAccount(address);
+
+    if (account === null) {
+        account = new Account(address, [], []);
+    }
 
     //TODO Added line for testing, remove later
     res.set('Access-Control-Allow-Origin', '*').status(200).json(account);
