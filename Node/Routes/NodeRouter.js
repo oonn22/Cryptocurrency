ac1111c23bb2ce3b37da3288ff3f2cfe9a8bfcaa
oonn22 <dropthepuck12@gmail.com>
2022-01-14 14:06:34 -0500

    Now emits new nodes to network.

--- Node/Routes/NodeRouter.js
+++ Node/Routes/NodeRouter.js
@@ -7,13 +7,18 @@ async function addNode(req, res, next) {
     let network = req.app.locals.Network;
     let url = req.body.url;
 
-    let ping = await network.request.getDataFromNode(url, "/");
+    if (!network.nodes.hasNode(url)) {
+        let ping = await network.request.getDataFromNode(url, "/");
 
-    if (ping) {
-        network.addNode(url);
-        res.status(200).send();
+        if (ping) {
+            network.addNode(url);
+            await network.requests.postData("/nodes/node/add", {url: url});
+            res.status(200).json({msg: "Added to network."});
+        } else {
+            res.status(422).json({msg: "Could not reach node."});
+        }
     } else {
-        res.status(422).send();
+        res.status(409).json({msg: "Node already added."});
     }
 
     next();
