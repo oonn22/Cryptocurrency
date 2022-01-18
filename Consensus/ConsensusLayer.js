b5b91bcb7dd00dcb6ca0191071b459a26ed23f74
oonn22 <dropthepuck12@gmail.com>
2022-01-18 14:09:03 -0500

    Fixed bug where snowball was left in memory after finished.

--- Consensus/ConsensusLayer.js
+++ Consensus/ConsensusLayer.js
@@ -9,19 +9,23 @@ class ConsensusLayer {
     }
 
     /**
-     * Performs consensus on a block, coming to consensus on which block occupies the position of the inputted block
+     * Performs consensus on a block, coming to consensus on which block occupies the position of the inputted block.
+     * Block should be added to DAG immediately after.
      * @param {Block} block - the inputted block
      * @returns {Promise<Block>} - -1 implies consensus already underway
      */
     async conformOnBlock(block) {
         let snowballID = block.previousHash;
+        let result;
 
         if (snowballID === ENCODED_256_ZERO_BITS)
             snowballID += block.sender;
 
         if (!this.snowballs[snowballID]) {
             this.snowballs.set(snowballID,  new Snowball(block, this.network));
-            return await this.snowballs.get(snowballID).consensus();
+            result = await this.snowballs.get(snowballID).consensus();
+            this.snowballs.delete(snowballID);
+            return result;
         }
     }
 
